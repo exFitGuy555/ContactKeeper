c7be1e7b56e8cce8709d9dacd3951ea4b9396aa9
exFitGuy555 <guy.finkelshtein5@gmail.com>
2020-08-11 22:22:30 +0300

    Removed initial Route

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ connectDb();
 //init Middelware
 app.use(express.json({ extended: false}))
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to Contact Keeper Api'})
-});
-
-
 //Define Routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
